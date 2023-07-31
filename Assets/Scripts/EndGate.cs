781d9cf8c20322b446ebaf7525cc9848a2c3a35f
AJYez <amyezikjr@email.neit.edu>
2023-07-31 14:41:28 -0400

    7/31
    7/31

--- Assets/Scripts/EndGate.cs
+++ Assets/Scripts/EndGate.cs
@@ -2,14 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class EndGate : MonoBehaviour
 {
     public GameObject messageText;
+    public GameObject cutAwayCamera;
+    public bool GameOver = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (GameOver)
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                //Restart Game
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         messageText.SetActive(true);
-        messageText.GetComponent<TMP_Text>().text = "You Win!";
+        messageText.GetComponent<TMP_Text>().text = "You Win! \n Press Enter to play again";
+        if(other.gameObject.tag == "Player")
+        {
+            other.GetComponent<CharacterController>().enabled = false;
+            StartCoroutine(ChangeCamera());
+        }
+    }
+
+    IEnumerator ChangeCamera()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GameOver = true;
+        GameObject.Find("MainCamera").SetActive(false);
+        cutAwayCamera.SetActive(true);
     }
 }
